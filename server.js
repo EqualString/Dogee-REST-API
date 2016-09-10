5f49a52c4a21712cd8b1329ad67046f26f630224
EqualString <alen.egredzija@gmail.com>
2016-09-10 17:11:17 +0200

    v1.2

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //Dopunski nodejs moduli
-var express = require('express');
-var fs = require('fs');
-var app = express();
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var jsonfile   = require('jsonfile');
+
+var file = 'data/api-data.json';
+var app_token = '6WjVwnhfh9m2jT385X21aJrL5z0zM375';
 
 //Konfiguracija porta za aplikaciju (local & Heroku)
 var server_port = process.env.PORT || 8080 ;
@@ -9,17 +13,59 @@ var server_port = process.env.PORT || 8080 ;
 //Konfiguracija engine-a
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
-app.set('views', __dirname + '/public');
+app.set('views', __dirname + '/data');
 
-/** API ruta **/
-app.get('/', function(req, res){
+app.use(bodyParser.json()); // support za json encoded body
+app.use(bodyParser.urlencoded({ extended: true })); // support za encoded body
 
-	//Sinkrono čitanje datoteke i slanje 
-	var json = JSON.parse(fs.readFileSync('public/api-data.json', 'utf8'));
-	res.send(json);
+/** API **/
+app.post('/login', logIn);
+app.post('/logout', tokenTest, function(req,res){
+
+	res.send('');
 	res.end();
 	
 });
+app.post('/mydog', tokenTest, function(req,res){
+
+	var json = jsonfile.readFileSync(file); //Dohvati json
+	
+	res.send(json); //Pošalji cijeli json
+	res.end();
+
+});
+
+
+//Dodatne funkcije
+function logIn(req,res) {
+	
+	var json = jsonfile.readFileSync(file); //Dohvati json
+	
+	if ((json.user.username == req.body.username)&&(json.user.password == req.body.password)){
+		res.send(app_token); //Vrati token
+		res.end();
+	}
+	else {
+		res.send('wrong-login');
+		res.end();
+	}
+	
+}
+
+function tokenTest(req, res, next){
+
+	if(req.body.token == app_token){
+		
+		next();
+		
+	}else{
+	
+		res.send('token-error');
+		res.end();
+		
+	} 
+	
+}	
 
 //Konfiguracija servera
 var server = app.listen(server_port, function () {
@@ -28,5 +74,5 @@ var server = app.listen(server_port, function () {
 	console.log('app @ :http://localhost:8080/');
 });
 
-app.use(express.static(__dirname + '/public'));//Koristi sve iz folder 'public'
+app.use(express.static(__dirname + '/data'));//Koristi sve iz folder 'data'
 
